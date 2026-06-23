999694e01f114279620b87438f1452cfaeff703c
hcru77 <h.cruz1@ufl.edu>
2026-06-22 22:48:42 -0400

    winsock initialization

--- ChatClient/main.cpp
+++ ChatClient/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <WinSock2.h>
+
+
+
 
 int main() {
-	std::cout << "yello" << std::endl;
+	WSADATA wsaData;
+
+	// Requesting socket version 2.2 from OS
+	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+	
+	if (result != 0) {
+		std::cerr << "WSAStartup failed with code: " << result << std::endl;
+		return 1;
+	}
+
+	std::cout << "Winsock initialized successfully . " << std::endl;
+
+	std::cout << "press enter " << std::endl;
+	std::cin.get();
+
+
+	WSACleanup();
+	std::cout << "winsock cleaned" << std::endl;
 	return 0;
 }
--- ChatServer/main.cpp
+++ ChatServer/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <WinSock2.h>
+
 
 int main() {
-	std::cout << "this the server main" << std::endl;
+	
+	WSADATA wsaData;
 
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (result != 0) {
+		std::cerr << "WSA failed with code" << result << std::endl;
+		return 1;
+	}
+
+	std::cout << "WSA started successfully! " << std::endl;
+
+	std::cout << "press enter" << std::endl;
+	std::cin.get();
+
+
+	WSACleanup();
+	std::cout << "Winsock cleaned successfully";
 	return 0;
 }
