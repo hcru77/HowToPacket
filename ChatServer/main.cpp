f841d72a5e547490839c5624eea6da037fc4c47c
hcru77 <h.cruz1@ufl.edu>
2026-06-28 23:45:40 -0400

    finished servre starter code

--- ChatServer/main.cpp
+++ ChatServer/main.cpp
@@ -4,13 +4,19 @@
 #include <stdio.h>
 
 #define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
 
 
 int main() {
 	WSADATA wsaData;
 
-	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	int iResult; 
+	int iSendResult;
 
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
 		std::cerr << "WSA failed with code" << iResult << std::endl;
 		WSACleanup();
@@ -21,7 +27,7 @@ int main() {
 
 
 	// ptr is temp for iterating IP addresses
-	struct addrinfo* result = NULL, * ptr = NULL, hints = {};
+	struct addrinfo* result = NULL, *ptr = NULL, hints = {};
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
@@ -36,20 +42,86 @@ int main() {
 		return 1;
 	}
 
-	SOCKET listenSocket = INVALID_SOCKET;
-	listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	SOCKET ListenSocket;
+	ListenSocket = INVALID_SOCKET;
 
-	if (listenSocket == INVALID_SOCKET) {
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
 		std::cerr << "Error at socket(): " << WSAGetLastError();
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
 
-	std::cout << "press enter" << std::endl;
-	std::cin.get();
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult != 0) {
+		std::cerr << "binding failed with code: " << iResult << std::endl;
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result); // address information is no longer needed after the bind
+
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		std::cerr << "listen failed with error: " << SOCKET_ERROR << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ClientSocket;
+	ClientSocket = INVALID_SOCKET;
+
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		std::cerr << "accept failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ListenSocket);
+
+	do {
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			std::cout << "bytes received: " << iResult << std::endl;
+
+			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+			if (iSendResult == SOCKET_ERROR) {
+				std::cerr << "send failed: " << WSAGetLastError() << std::endl;
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+			std::cout << "bytes sent: " << iSendResult << std::endl;
+		}
+		else if (iResult == 0) {
+			std::cout << "connection closing... " << std::endl;
+		}
+		else {
+			std::cerr << "recv failed" << WSAGetLastError() << std::endl;
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+	} while (iResult > 0);
+
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		std::cerr << "shutdown failed" << WSAGetLastError() << std::endl;
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
 
 
+	//std::cout << "press enter" << std::endl;
+	//std::cin.get();
+
+	closesocket(ClientSocket);
 	WSACleanup();
 	std::cout << "Winsock cleaned successfully";
 	return 0;
