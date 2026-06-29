78c03ff862e04ae26c0a741407f7a691f2d31832
hcru77 <h.cruz1@ufl.edu>
2026-06-29 00:15:17 -0400

    finished client connect to server

--- ChatClient/main.cpp
+++ ChatClient/main.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
+#include <stdio.h>
 #include <WinSock2.h>
+#include <ws2tcpip.h>
 
 
-int main() {
+#define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
+
+int main(int argc, char* argv[]) {
 	WSADATA wsaData;
 
-	// Requesting socket version 2.2 from OS
-	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
-	
-	if (result != 0) {
-		std::cerr << "WSAStartup failed with code: " << result << std::endl;
+	struct addrinfo* result = NULL, * ptr = NULL, hints = {};
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	int iResult;
+
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) {
+		std::cerr << "WSAStartup failed with code: " << iResult << std::endl;
+		return 1;
+	}
+	std::cout << "Winsock initialized successfully . " << std::endl;
+
+
+	iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		std::cerr << "getaddrinfo failed with code: " << iResult << std::endl;
+		WSACleanup();
 		return 1;
 	}
 
-	std::cout << "Winsock initialized successfully . " << std::endl;
+	SOCKET ConnectSocket;
+	ConnectSocket = INVALID_SOCKET;
+	ptr = result;
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		std::cerr << "error at socket: " << WSAGetLastError() << std::endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
 
-	std::cout << "press enter " << std::endl;
-	std::cin.get();
+	iResult = connect(ConnectSocket, ptr->ai_addr, ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
 
+	// Normally should try the next address from getaddrinfo
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		std::cerr << "connection to server failed!";
+		WSACleanup();
+		return 1;
+	}
 
 	WSACleanup();
 	std::cout << "winsock cleaned" << std::endl;
